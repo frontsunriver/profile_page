d2f38dd50079b366ea7b003e707b4607ebc6325c
Sunriver <seriousman217winter@gmail.com>
2025-10-01 18:16:47 -0400

    feat(:update)Update vercel issue

---
+++ copy-build.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create public directory if it doesn't exist
+if (!fs.existsSync('public')) {
+  fs.mkdirSync('public');
+}
+
+// Copy all files from out to public
+function copyDir(src, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+  
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+  
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    
+    if (entry.isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+// Copy out directory to public
+copyDir('out', 'public');
+console.log('Build files copied to public directory');
